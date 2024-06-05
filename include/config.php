a127bd57bad50c92e5eb1b1aa1287d4318f9d38e
MaZeLxSx <89730726+MaZeLxSx@users.noreply.github.com>
2024-06-05 19:12:46 +0700

    Add files via upload

---
+++ include/config.php
@@ -0,0 +1,5 @@
+<?php
+$host     = "localhost";    // Nama host
+$username = "id22048209_sidowayah";         // Username database
+$password = "Sidowayah12345#";   // Password database
+$database = "id22048209_sidowayah";   // Nama database
